e4986513769eed6f4cdb9590b4fc277327c34d0c
lidya-toscano <75463460+lidya-toscano@users.noreply.github.com>
2021-05-05 21:18:25 -0700

    created the search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from "./logo.svg";
+import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Lidya's Dictionary</h1>
+      <header className="App-header">
+        <h1>Lidya's Dictionary</h1>
+      </header>
+
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer className="text-centered">
+        <h5>
+          <a
+            href="https://github.com/lidya-toscano/dictionary-project"
+            target="nonreferrer"
+          >
+            Open-Source Code
+          </a>{" "}
+          by{" "}
+          <a href="https://www.lidyatoscano.com/" target="nonreferrer">
+            Lidya Toscano
+          </a>
+        </h5>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDeafault();
+    alert(`Searching for ${keyword}...`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onCHange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
