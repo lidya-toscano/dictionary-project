f350eabebe5f110dc356922f8e7a0ca93be897c4
lidya-toscano <75463460+lidya-toscano@users.noreply.github.com>
2021-05-16 12:34:17 -0700

    added the photo API, fine tuning of styling, and fixed the search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    console.log();
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000135769e70fe0f4e1bbcf19d142d269b23";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
@@ -43,6 +57,7 @@ export default function Dictionary(props) {
         </form>
         <div className="Hint">Suggested words: beer, pizza, oreo, pet...</div>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
